5b40011b668695179966f450ce66433d3f260524
Aecio Junior <C0654546@vale.com>
2023-05-02 10:43:41 -0300

    Add class Departamentos

--- empresas/models.py
+++ empresas/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=50, help_text='Nome da empresa')
+
+    def __str__(self):
+        return self.nome
